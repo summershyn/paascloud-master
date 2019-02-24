01f1aadc512a40b091ce521f25b4ec5c1d481488
Jason-Hsia <457400978@qq.com>
2019-02-24 20:59:10 +0800

    代码检查

---
+++ docker/schema.sql
@@ -0,0 +1,5 @@
+CREATE  database paascloud_mdc;
+CREATE  database paascloud_omc;
+CREATE  database paascloud_opc;
+CREATE  database paascloud_tpc;
+CREATE  database paascloud_uac;
---
+++ elastic-job-lite-starter/elastic-job-lite-starter-demo/src/main/java/com/paascloud/elastic/demo/Foo.java
@@ -0,0 +1,18 @@
+package com.paascloud.elastic.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * The class Foo.
+ *
+ * @author paascloud.net @gmail.com
+ */
+@Data
+@AllArgsConstructor
+public class Foo {
+	/**
+	 * The Id.
+	 */
+	private Long id;
+}
